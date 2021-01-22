61cb11da22ffc23c8def88c940c6e69c02c7ba7c
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2021-01-22 13:59:56 -0500

    making form

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,41 @@
 import axios from 'axios';
 
+export const API_CALL_START = "API_CALL_START"
+export const API_CALL_FINISH = "API_CALL_FINISH"
+export const ADD_NEW_SMURF = "ADD_NEW_SMURF"
+export const ERROR_TEXT = "ERROR_TEXT"
+
+const fetchSmurfs = () => (dispatch) => {
+    dispatch({ type: API_CALL_START });
+    console.log(dispatch({ type: API_CALL_START }));
+    axios
+        .get('http://localhost:3333/smurfs')
+        .then((res) => {
+            console.log(res.data)
+            dispatch({ type: API_CALL_FINISH, payload: res.data});
+            console.log(dispatch({ type: API_CALL_FINISH, payload: res.data}))
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    
+}
+
+const addSmurf = (name) => (dispatch) =>{
+    dispatch({ type: ADD_NEW_SMURF, payload:name})
+    axios.
+    post('http://localhost:3333/smurfs')
+    .then((res) => {
+        console.log(res)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+const errrorText = dispatch =>{
+    dispatch({ type:ERROR_TEXT, })
+}
 //Task List:
 //1. Add fetch smurfs action: 
 //              - fetch and return initial list of smurfs
